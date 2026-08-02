b8b9be1f1d50817f69922ab3568394c026feb58c
Sangram987 <routsangram320@gmail.com>
2026-08-02 23:31:26 +0530

    Learned DataTypes

---
+++ DataTypes/PracticeDataType.js
@@ -0,0 +1,34 @@
+//Q1:- Store your age as a number.
+let age = 19;
+console.log(age);
+
+
+//Q2:- Store whether you are a student using a boolean.
+let bool = Boolean("I am a student")
+console.log(bool)
+
+
+//Q3:- Create an array of 5 favorite colors.
+let arr = [
+    "black",
+    "white",
+    "green",
+    "Light_Gray",
+    "Navy_Blue"
+]
+console.log(arr);
+
+//Q4:- Create an object with name, age, and city.
+let createObj = {
+    name: "Sangram KEshari Rout",
+    age: 18,
+    city: "Jajpur"
+}
+console.log(createObj);
+
+
+//Q5: -Print the type of all the above variables using typeof.
+console.log(typeof (age));
+console.log(typeof (bool));
+console.log(typeof (arr));
+console.log(typeof (createObj));
