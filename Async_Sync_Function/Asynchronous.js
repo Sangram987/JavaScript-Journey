af6c4d79c23e6bbfd156d988fa8863e9abfa7e1a
Sangram987 <csemunu@gmail.com>
2026-08-09 00:52:21 +0530

    Learned Asynchronous and synchronous function

---
+++ Async_Sync_Function/Asynchronous.js
@@ -0,0 +1,5 @@
+console.log("Start");
+setTimeout(() => {
+    console.log("After 2 seconds run it.");
+}, 2000);
+console.log("End")
---
+++ Async_Sync_Function/Synchronous.js
@@ -0,0 +1,7 @@
+const task1 = "Make a project";
+const task2 = "Make a ppt of the project";
+const task3 = "Submit the project";
+
+console.log(task1);
+console.log(task2);
+console.log(task3);
